29ccdb11bf33de4e12807f1725d062b6ba52bd63
TheAlmightyHelix <ccyyhh97@hotmail.com>
2023-09-19 12:26:52 -0500

    extact doglist instructions as a component

---
+++ src/components/DogListInstructions.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { interactableColors } from "../lib/styles";
+import Button from "./atomic/Button";
+import PopupModal from "./atomic/PopupModal";
+
+export default function DogListInstructions() {
+    const [displayInstructions, setDisplayInstructions] = useState(false)
+
+    return (
+        <>
+            <Button
+                additionalStyling={interactableColors.proceed}
+                onclick={() => { setDisplayInstructions(true) }}
+            >
+                😵‍💫
+            </Button>
+            {displayInstructions &&
+                <PopupModal
+                    title="How it works"
+                    close={() => { setDisplayInstructions(false) }}
+                >
+                    <div className="p-3">
+                        <p>1. Enter some criteria to search for the type of dogs you want to adopt </p>
+                        <p>2. Select the ones you might consider adopting</p>
+                        <p>3. Once you are ready, click the "FIND A MATCH" button</p>
+                    </div>
+                </PopupModal>}
+        </>
+    )
+}
